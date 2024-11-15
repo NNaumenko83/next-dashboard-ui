738900905d1e2462c268729bacdb71f77da3b896
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-11-15 16:45:10 +0200

    [Add] Menu and Logo Link

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Menu from '@/components/Menu'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function DashboardLayout({
     children,
 }: Readonly<{
@@ -6,7 +10,13 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
             {/* LEFT */}
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  bg-amber-300">l</div>
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  bg-amber-300 p-4">
+                <Link href="/" className="flex items-center gap-x-2 justify-center lg:justify-start">
+                    <Image src="/logo.png" alt="Logo" width={32} height={32} />
+                    <span className="hidden lg:block">DevSchool</span>
+                </Link>
+                <Menu />
+            </div>
 
             {/* RIGHT */}
             <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-lime-300">r</div>
